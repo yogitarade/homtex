62d363d412c7852b4fa1486f30d6fcceedf1039e
yogitarade <yogitarade@gmail.com>
2017-05-20 12:59:43 +0530

    Add files via upload

---
+++ application/config/twilio.php
@@ -0,0 +1,44 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	/**
+	* Name:  Twilio
+	*
+	* Author: Ben Edmunds
+	*		  ben.edmunds@gmail.com
+	*         @benedmunds
+	*
+	* Location:
+	*
+	* Created:  03.29.2011
+	*
+	* Description:  Twilio configuration settings.
+	*
+	*
+	*/
+
+	/**
+	 * Mode ("sandbox" or "prod")
+	 **/
+	$config['mode']   = 'sandbox';
+
+	/**
+	 * Account SID
+	 **/
+	$config['account_sid']   = 'AC2ed902e78cda1a9718a5ebee7879b7bd';
+
+	/**
+	 * Auth Token
+	 **/
+	$config['auth_token']    = '31007bc90c68ef95bc9e17f0462f3c7d';
+
+	/**
+	 * API Version
+	 **/
+	$config['api_version']   = '2010-04-01';
+
+	/**
+	 * Twilio Phone Number
+	 **/
+	$config['number']        = '9182182225';
+
+
+/* End of file twilio.php */
